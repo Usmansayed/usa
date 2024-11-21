e1fd0dd3c688ba7a9b3cebfecc8a3a25be2bf7eb
Muhammad Usman Sayed <usmansayed789@gmail.com>
2024-11-21 20:02:11 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,11 +1,6 @@
-
-
-public class pgm1 {
-		
-		public static void main(String[] args) {
-	
-
-					System.out.println("hello i am usman sayed");
-	}
-
+// HelloWorld.java
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
